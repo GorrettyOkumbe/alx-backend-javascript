4ab845b6f8b43761d9f59baac5be562e90744eb6
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-12-19 22:35:00 +0300

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  task = false;
-  task2 = true;
+  var task;
+  var task2;
 
   if (trueOrFalse) {
-    var task;
-    var task2;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
